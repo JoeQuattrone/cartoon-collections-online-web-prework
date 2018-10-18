3ff23067f6d9b5706f44b1c481e916f7069b8d76
Joe Quattrone <joequattrone5@gmail.com>
2018-10-18 19:39:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.join(", ")
-  array.include?(cheese_types)
+  array.include?("cheddar", "gouda", "camembert")
 end
